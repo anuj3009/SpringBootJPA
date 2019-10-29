3c43cf42dd32b0fdba49d6ed37b8fec211da5472
Anuj Patait <anuj3009@gmail.com>
2019-10-29 13:59:12 -0600

    initial setup

---
+++ src/main/java/com/anuj/studyJAVA/springBoot/SpringBootJPA/SpringBootJPA/SpringBootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.studyJAVA.springBoot.SpringBootJPA.SpringBootJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuj/studyJAVA/springBoot/SpringBootJPA/SpringBootJPA/SpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.studyJAVA.springBoot.SpringBootJPA.SpringBootJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
